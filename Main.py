445a0a3aecc81394098098177998f4f9c2e2014e
marciomacielbastos <marciomacielbastos@hotmail.com>
2015-10-19 09:32:36 -0400

    Handle exceptions in RealEstateSettings

--- Main.py
+++ Main.py
@@ -5,9 +5,5 @@ from RealEstateSettings import RealEstateSettings
 
 __author__ = 'marcio'
 
-# res = RealEstateSettings()
-# res.get_coordinates_csv()
-a = [1, 2, 3]
-Normalizer.set_tuple(0, a)
-print a
-print CsvManager.get_number_of_rows('/home/marcio/marcio/boroughs/brooklyn.csv')
+res = RealEstateSettings()
+res.get_coordinates_csv('/home/marcio/marcio/boroughs/bronx.csv', 'coord_bronx.csv')
