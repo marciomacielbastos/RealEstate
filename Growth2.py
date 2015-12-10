1525b37fe21cb54e95af330187693aab61599be2
marciomacielbastos <marciomacielbastos@hotmail.com>
2015-12-10 01:26:46 -0500

    Growth2

--- Growth2.py
+++ Growth2.py
@@ -1,14 +1,10 @@
 from CsvManager import CsvManager
-from ErrorLog import ErrorLog
-from Progress import Progress
-import numpy as np
-import datetime
 import threading
 
 __author__ = 'marcio'
 
 
-class Growth:
+class Growth2:
     def __init__(self, inputdata, outputdata, path):
         self.path = path
         self.input = CsvManager.read(inputdata)
@@ -23,20 +19,25 @@ class Growth:
             bbls.append(int(bbl[0]))
         return bbls
 
-    def binary_search(self, bbl, begin, end):
-        if bbl < self.input[(end - begin)/2][0]:
-            end = (end-begin)/2
-            self.binary_search(bbl, begin, end)
-        elif bbl > self.input[(end - begin)/2][0]:
-            begin = (end - begin)/2
-            self.binary_search(bbl, begin, end)
-        elif bbl == self.input[(end - begin)/2][0]:
-            return self.sequential_search(bbl, (end - begin) / 2)
-
     def sequential_search(self, bbl, index):
         while bbl == self.input[index][0]:
             index -= 1
         return index+1
+
+    def binary_search(self, bbl, begin, end):
+        cond = len(self.input)
+        while cond:
+            index = (end - begin)/2 + begin
+            val = self.input[index][0]
+            if bbl < val:
+                end = (end-begin) / 2 + begin
+                cond /= 2
+            elif bbl > val:
+                begin = begin + (end - begin)/2 + 1
+                cond /= 2
+            elif bbl == val:
+                return self.sequential_search(bbl, index)
+        return None
 
     def make_time_series(self, bbl, index):
         ipt = self.input[self.ts[index]]
@@ -48,6 +49,8 @@ class Growth:
                 dst[su].append((ipt[2], ipt[3]))
             else:
                 dst[su] = [(ipt[2], ipt[3])]
+            if self.ts[index] < len(self.input):
+                ipt = self.input[self.ts[index]]
         series = [[key, dst[key]] for key in dst.keys()]
         return series
 
@@ -61,25 +64,33 @@ class Growth:
     def preprocess(self, index):
         period = len(self.input) / self.kernels
         offset = len(self.input) % self.kernels
+        end = 0
+        begin = 0
+        for i in xrange(index+1):
+            begin = end
+            end += period
+            if offset:
+                end += 1
+                offset -= 1
+        for i in xrange(begin, end):
+            self.input[i] = (self.fix(self.input[i][0], 0), self.input[i][1], self.input[i][2], self.fix(self.input[i][3], 3))
+            bola= 2
 
-        pass
 
     def mount_table(self, bbls, index):
+        self.preprocess(index)
         time_series = []
-        self.ts[index] = len(self.input)-1
-        self.ts[index] = self.binary_search(bbls[len(bbls)-1], 0, self.ts[index])
+        self.ts[index] = self.binary_search(bbls[0], 0, len(self.input)-1)
         while bbls:
             bbl = bbls.pop(0)
             time_series.append([bbl, self.make_time_series(bbl, index)])
         self.ts[index] = time_series
+        print "terminou ",index
 
     def get_avg_ts(self):
         bbls = self.get_bbls()
         offset = len(bbls) % self.kernels
-        if offset:
-            period = (len(bbls) / self.kernels)
-        else:
-            period = len(bbls) / self.kernels
+        period = len(bbls) / self.kernels
         end = 0
         t = []
         for i in xrange(self.kernels):
@@ -96,8 +107,10 @@ class Growth:
         for i in xrange(len(t)):
             t[i].start()
         for i in xrange(len(t)):
+            print "quase"
             t[i].join()
         time_series = []
+        print "quase"
         for ts in self.ts:
             time_series += ts
         return time_series
--- Main.py
+++ Main.py
@@ -1,26 +1,9 @@
 # -*- coding: utf-8 -*-
+from Growth2 import Growth2
 
 __author__ = 'marcio'
 
-# res = Growth('/tmp/AcrisInPluto.csv', '/home/marcio/Marcio/timeseries.csv', '/tmp/bbs.csv', 3, 6)
-# res.growth(6)
-a = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
-kernel = 4
-offset = len(a) % kernel
-if offset:
-    t = (len(a) / kernel)
-else:
-    t = len(a) / kernel
-end = 0
-for i in xrange(kernel):
-    begin = end
-    end = end+t
-    if offset:
-        end += 1
-        offset -= 1
-    k = a[begin:end]
-    if end > len(a):
-        end = len(a)
-    print k
+res = Growth2('/tmp/t1.csv', '/home/marcio/Marcio/timeseries.csv', '/tmp/t2.csv')
+res.growth(1)
 
 
